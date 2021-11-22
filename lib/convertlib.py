7eb5a731456f2907b06d2541e7dbd729cbcaced7
Joe Kerrigan <joek1301@gmail.com>
2021-11-22 21:28:38 +0000

    Add library support for converting to new playlist editor

--- lib/convertlib.py
+++ lib/convertlib.py
@@ -2,6 +2,10 @@
 """
 
 import validators
+import typing
+import csv
+import spotipy
+import util
 import re
 
 
@@ -53,13 +57,96 @@ def extract_playlist_id_from_url(url: str) -> str:
     Raises:
         ValueError: if url is not a valid Spotify playlist URL.
     """
+    # confirm that the url argument is actually a URL
     if validators.url(url):
+        # confirm that it is a spotify playlist URL
         match = SPOTIFY_PLAYLIST_URL_VALIDATOR.findall(url)
 
         if match:
-            return match[0]
+            return match[0]  # ... and extract the ID portion of the URL
         else:
             raise ValueError(
                 "Argument url must be a valid Spotify playlist URL")
     else:
         raise ValueError("Argument url must be a valid URl")
+
+
+def write_new_playlist_csv(spotify: spotipy.Spotify, playlist_id: str, stream: typing.TextIO) -> typing.List[str]:
+    """Converts a Spotify playlist with the given ID into a CSV file, formatted for WTJU's new playlist editor.
+    Writes the output to a given file-like object, and returns a list of human-readable warnings in case individual tracks
+    could not be converted.
+
+    Args:
+        spotify (spotipy.Spotify): Spotify API client instance to use for API requests.
+        playlist_id (str): Spotify playlist ID of the playlist to convert.
+        stream (typing.TextIO): File-like object to write the converted CSV to.
+
+    Returns:
+        typing.List[str]: List of human-readable warnings
+    """
+
+    writer = csv.writer(stream)
+    warnings = []
+    # write the CSV header row
+    writer.writerow(NEW_EDITOR_HEADERS)
+
+    # make API call for the playlist's items (i.e., its tracks)
+    playlist_items = spotify.playlist_items(playlist_id)
+
+    # TODO: >100 tracks in a playlist? (spotipy uses limit=100 by default)
+    # responses are paginated
+    for item in playlist_items["items"]:
+        # fetch relevant variables
+        track = item["track"]
+        # is this track local? (imported from personal library)
+        is_local_track = track["is_local"]
+        artist = track["artists"][0]  # just get the first artist on the track
+        track_name = track["name"]
+
+        album = track["album"]  # get album info
+        album_name = album["name"]  # name of the album
+        artist_name = artist["name"]  # name of the artist
+
+        label = None
+        release_year = None
+        # we can only get additional album info on non-local tracks.
+        # Spotify doesn't carry extended album info for imports from
+        # local library.
+        if not is_local_track:
+            # get additional info for album
+            album_ext = spotify.album(album["id"])
+            label = album_ext["label"]  # record label that published the album
+            # get the first four chars of release date
+            release_year = album["release_date"][0:4]
+        else:
+            # we can't get this info ourselves, so it'll just have to be filled in manually.
+            # TODO: music database?
+            label = ""
+            release_year = ""
+            # add warning
+            warnings.append(
+                f"Track '{track_name}' was imported from your library, so I can't find its record label or release year automatically. Other fields for this song may be missing or incomplete. You'll have to enter this track's information into the station's interface manually.")
+
+        duration_ms = int(track["duration_ms"])
+        duration_minutes, duration_seconds = util._duration_from_ms(
+            duration_ms)
+
+        # pad out seconds so (3, 2) -> "3:02"
+        duration_str = f"{duration_minutes}:{str(duration_seconds).zfill(2)}"
+
+        row = [
+            track_name,  # track title
+            duration_str,  # track duration
+            artist_name,  # artist name
+            album_name,  # album name
+            release_year,  # release year
+            label,  # record label
+            "",  # composer
+            ""  # notes
+        ]
+
+        # write the row
+        writer.writerow(row)
+
+    # return any warnings we encountered
+    return warnings
--- lib/test.py
+++ lib/test.py
@@ -1,14 +1,16 @@
+import util
 import unittest
 import convertlib
 import spotipy
 import os
+import io
 
 SPOTIFY_API_CLIENT_ID = os.environ["SPOTIFY_API_CLIENT_ID"]
 SPOTIFY_API_CLIENT_SECRET = os.environ["SPOTIFY_API_CLIENT_SECRET"]
 
 spotify_creds = spotipy.SpotifyClientCredentials(
     client_id=SPOTIFY_API_CLIENT_ID, client_secret=SPOTIFY_API_CLIENT_SECRET)
-spotify = spotipy.Spotify(auth=spotify_creds)
+spotify = spotipy.Spotify(client_credentials_manager=spotify_creds)
 
 
 class TestPlaylistConverter(unittest.TestCase):
@@ -38,7 +40,37 @@ class TestPlaylistConverter(unittest.TestCase):
         self.assertRaises(ValueError, invalidUrl1)
         self.assertRaises(ValueError, invalidUrl2)
         self.assertRaises(ValueError, invalidUrl3)
-    pass
+
+    def test_new_playlist_converter(self):
+        # makes sure there are no warnings/errors when converting playlist
+        playlist_url = "https://open.spotify.com/playlist/1LOp2ieUmspXzw61pQ4LCi?si=0909c61a78814603"
+        playlist_id = convertlib.extract_playlist_id_from_url(playlist_url)
+
+        # allocate buffer to contain the converted csv
+        buffer = io.StringIO()
+        warnings = convertlib.write_new_playlist_csv(
+            spotify, playlist_id, buffer)
+
+        # make sure there are no warnings
+        self.assertEqual(warnings, [])
+
+
+class TestUtil(unittest.TestCase):
+    def test_duration_converter(self):
+        three_minutes_fortytwo_seconds = 3 * 60 * 1000 + 42 * 1000 + 33
+        # three minutes, forty two seconds, 33 ms = 3:42
+
+        self.assertEqual(util._duration_from_ms(
+            three_minutes_fortytwo_seconds), (3, 42))
+
+        sixty_seconds = 60 * 1000
+        # sixty seconds = one minute = 1:00
+
+        self.assertEqual(util._duration_from_ms(sixty_seconds), (1, 0))
+
+        ten_seconds = 10 * 1000
+        # ten seconds = 0:10
+        self.assertEqual(util._duration_from_ms(ten_seconds), (0, 10))
 
 
 if __name__ == "__main__":
