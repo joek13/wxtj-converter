c13793a9d2a5c5935b11ae94397a4d188e8cee8f
Joe Kerrigan <joek1301@gmail.com>
2021-11-24 00:27:35 +0000

    Switch between prod/dev API endpoints in dev/prod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,12 @@ import axios from "axios";
 
 import Main from "./Main";
 
-axios.defaults.baseURL = "https://mlnaw218id.execute-api.us-east-1.amazonaws.com";
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = "https://mlnaw218id.execute-api.us-east-1.amazonaws.com";
+} else {
+  axios.defaults.baseURL = "https://k3eycnkg18.execute-api.us-east-1.amazonaws.com/";
+}
+
 
 function App() {
   return (<Main />);
