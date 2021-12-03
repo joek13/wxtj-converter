a7f0f7217a9228b4c25710b9307bb75ac4dcf84a
Joe Kerrigan <joek1301@gmail.com>
2021-12-03 23:37:10 +0000

    Tweak footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -6,8 +6,7 @@ import { Container } from "react-bootstrap";
 export default function Footer() {
     return <footer>
         <Container className="bg-light text-center p-5" fluid>
-            <p>Made with love by Joe Kerrigan</p>
-            <p>Check out the <a href="https://github.com/joek13/wxtj-converter">source code</a></p>
+            <p>View the <a href="https://github.com/joek13/wxtj-converter">source code</a></p>
             <p>Bugs? Feedback or feature requests? Want me to listen to your show? Reach me at <a href="mailto:joek1301@gmail.com">joek1301@gmail.com</a></p>
         </Container>
     </footer>;
